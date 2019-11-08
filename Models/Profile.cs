b074696ec231f427a6d5af398e6c23853283e485
Nishant <gautamnishant3392@gmail.com>
2019-11-08 09:52:26 +0100

    Create profile model,T2S3

---
+++ Models/Profile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    [DataContract]
+    class Profile
+    {
+        [DataMember]
+        public int ID { get; set; }
+        [DataMember]
+        public string FirstName { get; set; }
+        [DataMember]
+        public string LastName { get; set; }
+        [DataMember]
+        public String Description { get; set; }
+        [DataMember]
+        public decimal Longitude { get; set; }
+        [DataMember]
+        public decimal Latitude { get; set; }
+        [DataMember]
+        public string ProfilePicture { get; set; }
+        
+        public Profile (int id, string firstName, string lastName, string description, decimal longitude, decimal latitude, string profilePicture )
+        {
+            this.ID = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Description = description;
+            this.Longitude = longitude;
+            this.Latitude = latitude;
+            this.ProfilePicture = profilePicture;
+        }
+
+    }
+}
