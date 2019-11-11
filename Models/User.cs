25740677d6f3f73e40859451d347f0302693a318
Radu Matusa <matusaradu@gmail.com>
2019-11-11 09:51:35 +0100

    Create user model, T1S1

---
+++ Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Models
+{
+    [DataContract]
+    public class User
+    {
+        [DataMember]
+        public int ID { get; set; }
+        [DataMember]
+        public string Email { get; set; }
+        [DataMember]
+        public string UserName { get; set; }
+        [DataMember]
+        public string Password { get; set; }
+        [DataMember]
+        public string Salt { get; set; }
+
+        public User(int id, string email, string userName, string password, string salt)
+        {
+            this.ID = id;
+            this.Email = email;
+            this.UserName = userName;
+            this.Password = password;
+            this.Salt = salt;
+        }
+    }
+}
