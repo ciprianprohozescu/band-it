46aec3cd95fe0b556d68b9c2c928200da31d4bcf
Andrei Mataoanu <amatao1999@gmail.com>
2019-11-15 10:07:31 +0100

    Create search bar, T1S2
    Fix Services UserController infinite recursive call error, T1S2
    
    
    Fix various errors + tests, T1S2
    
    
    Add desktop client, T1S2
    
    
    Fix naming problems, T1S2
    
    
    Fix ghost folders, T1S2

---
+++ ClientDesktop/UserIndex.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using User = Models.User;
+using ClientMVC.Models;
+
+namespace ClientDesktop
+{
+    public partial class UserIndex : Window
+    {
+        public UserIndex()
+        {
+            InitializeComponent();
+        }
+        private void Search_Button_Click(object sender, RoutedEventArgs e)
+        {
+            string search = SearchText.Text;
+            var client = new RestClient(ConfigurationManager.AppSettings.Get("APIURL"));
+            var request = new RestRequest("user", Method.GET);
+            request.AddParameter("search", search);
+            var content = client.Execute(request).Content;
+
+            var users = JsonConvert.DeserializeObject<List<User>>(content);
+
+            var model = new ClientMVC.Models.UserIndex();
+            model.Search = search;
+            model.Users = users;
+
+            dataGridUsers.ItemsSource = users;
+        }
+    }
+}
